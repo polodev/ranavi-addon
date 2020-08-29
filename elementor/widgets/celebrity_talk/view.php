53f9392cd020ea4eee0c6d0fc01881e2e7cec852
polodev <polodev10@gmail.com>
2020-08-29 22:20:15 +0600

    ______

--- elementor/widgets/celebrity_talk/view.php
+++ elementor/widgets/celebrity_talk/view.php
@@ -2,5 +2,51 @@
 namespace ibauthor\Polodev_WP_Companion;
 extract( $settings );
 ?>
-<span class="heading-intro"><?php echo esc_html( $title ); ?></span>
-<h2 class="heading-xl"><?php echo esc_html( $subtitle ); ?></h2>
+<?php 
+namespace ibauthor\Polodev_WP_Companion;
+extract( $settings );
+?>
+
+<section class="container-fluid no-gutters celebrity-talk-box">
+  <div class="container">
+      <div class="row">
+          <div class="col-12 text-center">
+              <h5 class="text-orange mt-0 mt-md-5">CELEBRITY</h5>
+              <h1 class="about-title mb-5">Talks About Us</h1>
+          </div>
+      </div>
+      <div class="row mb-5">
+          <div class="col-12 col-lg-6">
+              <div class="row">
+                  <div class="col-12 col-sm-6 celeb-image-wrap">
+                      <img class="img-fluid" src="https://www.eaglesdance.com/assets/images/celeb-talk/ferdous-ahmed.png" style="z-index: 4; position: absolute; left: 0px; top: 0px;" id="celeb-talk-photo">
+                      <div class="celeb-photo-box-red d-none d-xl-block"></div>
+                      <div class="celeb-photo-box-blue d-none d-xl-block"></div>
+                      <div class="celeb-photo-box-orange d-none d-xl-block"></div>
+                  </div>
+                  <div class="col-12 col-sm-6">
+                      <h4 class="about-title mt-3" id="celeb-talk-name">Ferdous Ahmed</h4>
+                      <h4 class="about-title mt-4" style="font-weight: 400" id="celeb-talk-profession">Actor</h4>
+                      <div class="card card-pink mt-4">
+                          <div class="card-body p-4">
+                              <p class="p-0 m-0" id="celeb-talk-comment"><small>Thank You!<br>Eagles Dance Company.<br>Great Experience With You. Best Wishes.</small></p>
+                          </div>
+                      </div>
+                      <div class="mt-5 pl-4">
+                          <button class="link-black" id="c-left"><i class="fa fa-2x fa-long-arrow-left"></i></button> &nbsp;&nbsp;&nbsp; <button class="link-black" id="c-right"><i class="fa fa-2x fa-long-arrow-right"></i></button>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div class="col-12 col-lg-6 d-none d-lg-block">
+              <div  id="celeb-talk-anim">
+                  <img class="img-celeb-anim celeb-anim-pos-1" data-key="1" src="https://www.eaglesdance.com/assets/images/ferdous-anim.png">
+                  <img class="img-celeb-anim celeb-anim-pos-2" data-key="3" src="https://www.eaglesdance.com/assets/images/sakib-anim.png">
+                  <img class="img-celeb-anim celeb-anim-pos-3" data-key="4" src="https://www.eaglesdance.com/assets/images/mehjzabin-anim.png">
+                  <img class="img-celeb-anim celeb-anim-pos-4" data-key="5" src="https://www.eaglesdance.com/assets/images/biddhya-anim.png">
+                  <img class="img-celeb-anim celeb-anim-pos-5" data-key="2" src="https://www.eaglesdance.com/assets/images/purnima-anim.png">
+              </div>
+          </div>
+      </div>
+  </div>
+</section>
